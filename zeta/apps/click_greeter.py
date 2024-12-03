84ed41a08624b079aa9ae3b3ef7711b5797c9ee5
Senkhosi Simelane <senkhxsi@gmail.com>
2024-12-03 10:50:36 +0200

    separated task 4 and 5 scripts

--- zeta/apps/click_greeter.py
+++ zeta/apps/click_greeter.py
@@ -1,23 +1,6 @@
-import os
-import glob
-from omegaconf import OmegaConf
 import click
-from scabha.schema_utils import clickify_parameters, paramfile_loader
-
-command = "greet"
-
-thisdir = os.path.dirname(__file__)
-
-# Load additional configurations
-source_files = glob.glob(f"{thisdir}/*.yaml")
-sources = [OmegaConf.load(item) for item in source_files]
-parserfile = os.path.join(thisdir, "greet.yaml")
-config = paramfile_loader(OmegaConf.load(parserfile), sources)[command]
-
 
 @click.command()
-#@click.argument('name')
-@clickify_parameters(config)
-def greet(**kwargs):
-    opts = OmegaConf.create(kwargs)
-    click.echo(f'Hello, {opts.name}!')
+@click.argument('name')
+def greet(name):
+    click.echo(f'Hello, {name}!')
